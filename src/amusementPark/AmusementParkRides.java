69c62893f46a1c8202f9799ecfcf84e3be4a7897
j1kahlon <kahlonjot94@gmail.com>
2023-11-16 09:20:07 +0530

    code changes in amusement park program

--- src/amusementPark/AmusementParkRides.java
+++ src/amusementPark/AmusementParkRides.java
@@ -2,9 +2,9 @@ package amusementPark;
 
 public class AmusementParkRides {
 	int height;
-	
+
 	void ridesAllowed() {
-		
+
 		if (height >= 90 && height <= 200)
 			System.out.println("Allowed to take Rides");
 		else {
--- src/amusementPark/MainaAmusementPark.java
+++ src/amusementPark/MainaAmusementPark.java
@@ -3,16 +3,14 @@ package amusementPark;
 import java.util.Scanner;
 
 public class MainaAmusementPark {
-	
 
 	public static void main(String[] args) {
 		AmusementParkRides amusementParkRides = new AmusementParkRides();
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter person height");
 
-amusementParkRides.height = sc.nextInt();
+		amusementParkRides.height = sc.nextInt();
 		amusementParkRides.ridesAllowed();
-		
 
 	}
 
